312a592cc41c728ae060a1d2cdb77a9f863ae6db
wensi <superwensiii@gmail.com>
2024-08-31 13:33:22 +0800

    first commit

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:9800000000"><i class="fas fa-phone"></i> +977 980 000 0000</a>
+         <a href="tel:9900000000"><i class="fas fa-phone"></i> +977 974 000 0000</a>
+         <a href="mailto:harshchy143@gmail.com"><i class="fas fa-envelope"></i> hahaha@gmail.com</a>
+         <a href="https://www.google.com/maps/place/Pasig,+Metro+Manila/@14.5788413,121.0399731,13z/data=!3m1!4b1!4m6!3m5!1s0x3397c7dc88f7b24f:0x4a592b2b4b34fd89!8m2!3d14.5736192!4d121.0784984!16zL20vMDFfOWRo?entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D"><i class="fas fa-map-marker-alt"></i> Pasig City</a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://www.facebook.com/wencywency.26/" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://twitter.com/HarshCh53587355" target="_blank"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="https://www.instagram.com/harshchy__/" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="https://www.linkedin.com/in/harsh-chaudhary-15763a150/" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>hahaha</span> | all rights reserved!</div>
+
+</footer>
